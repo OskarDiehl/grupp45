8bd97ffbb609c9b195a54008bc0a2eac8f5e8e21
ThanosSym <72200323+ThanosSym@users.noreply.github.com>
2020-12-15 21:21:48 +0100

    System class
    System class created

---
+++ Test/System.cpp
@@ -0,0 +1,8 @@
+//
+//  System.cpp
+//  
+//
+//  Created by Thanos Sym on 2020-12-15.
+//
+
+#include "System.hpp"
---
+++ Test/System.hpp
@@ -0,0 +1,13 @@
+//
+//  System.hpp
+//  
+//
+//  Created by Thanos Sym on 2020-12-15.
+//
+
+#ifndef System_hpp
+#define System_hpp
+
+#include <stdio.h>
+
+#endif /* System_hpp */
